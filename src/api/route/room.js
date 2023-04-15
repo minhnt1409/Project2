811b70f7812c7c65520d640bd3e49c576ee10303
minhnt200409 <khadungok123@gmail.com>
2023-04-16 01:53:32 +0700

    init rooms

--- src/api/route/room.js
+++ src/api/route/room.js
@@ -13,6 +13,8 @@ import connection from '../../db/connect.js';
 
 const router = express.Router();
 
+const JWT_SECRET = 'maBiMat';
+
 // Api lấy danh sách phòng
 router.get('/get_list_rooms', async (req, res) => {
     const token = req.body.token;
@@ -44,71 +46,111 @@ router.get('/get_room', async (req, res) => {
 router.post('/add_room', async (req, res) => {
     const { token, room_name, max } = req.body;
 
-    if (!token || !room_name) return callRes(res, responseError.PARAMETER_VALUE_IS_INVALID, null);
+    if (!token || !room_name) return callRes(res, responseError.PARAMETER_IS_NOT_ENOUGH, null);
 
     // Kiểm tra xác thực token của admin
-    const adminQueryString = `SELECT * FROM admins WHERE token = '${token}'`;
-
-    connection.query(adminQueryString, (err, adminRows, fields) => {
-        if (err) return callRes(res, responseError.UNKNOWN_ERROR, null);
-        if (adminRows.length > 0) {
-            // Thêm phòng mới vào database
-            const createRoomQueryString = `INSERT INTO rooms (room_name, max) VALUES ('${room_name}', '${max}')`;
-
-            connection.query(createRoomQueryString, (err, roomRows, fields) => {
-                if (err) return callRes(res, responseError.UNKNOWN_ERROR, null);
-
-                // Trả về thông tin phòng mới được tạo
-                const room_id = roomRows.insertId;
-                const getRoomQueryString = `SELECT * FROM rooms WHERE room_id = '${room_id}'`;
-
-                connection.query(getRoomQueryString, (err, rows, fields) => {
+    try {
+        const decoded = jsonwebtoken.verify(token, JWT_SECRET);
+        console.log(decoded);
+        const userId = decoded.userId;
+        const sql = 'SELECT * FROM users WHERE id = ?';
+        connection.query(sql, [userId], (err, results) => {
+            if (err) return callRes(res, responseError.UNKNOWN_ERROR, null);
+            if (results.length === 0) return callRes(res, responseError.USER_IS_NOT_VALIDATED, null);
+            if (results[0].role === 'admin') {
+                // Thêm phòng mới vào database
+                const createRoomQueryString = `INSERT INTO rooms (room_name,current, max, speed, author_id) VALUE (?, ?, ?, ?, ?)`;
+                
+                connection.query(createRoomQueryString, [room_name, 1, max, 1, decoded.userId], (err, Room, fields) => {
+                    console.log(err);
                     if (err) return callRes(res, responseError.UNKNOWN_ERROR, null);
-
-                    if (rows.length > 0) return callRes(res, responseError.OK, rows[0]);
-                    else return callRes(res, responseError.UNKNOWN_ERROR, null);
+    
+                    // Trả về thông tin phòng mới được tạo
+                    const room_id = Room.insertId;
+                    const getRoomQueryString = `SELECT * FROM rooms WHERE room_id = '${room_id}'`;
+    
+                    connection.query(getRoomQueryString, (err, rows, fields) => {
+                        if (err) return callRes(res, responseError.UNKNOWN_ERROR, null);
+    
+                        if (rows.length > 0) {
+                            let data = {
+                                room_id : rows[0].room_id,
+                                room_name : rows[0].room_name,
+                                current : rows[0].current,
+                                max : rows[0].max,
+                                speed : rows[0].speed,
+                                author : {
+                                    username : decoded.username,
+                                    id : decoded.userId
+                                },
+                                created : rows[0].created
+                            }
+                            return callRes(res, responseError.OK, data);
+                        }
+                        else return callRes(res, responseError.UNKNOWN_ERROR, null);
+                    });
                 });
-            });
-        } else return callRes(res, responseError.TOKEN_IS_INVALID, null);
-    });
+            }else return callRes(res, responseError.NOT_ACCESS,null);
+        });
+    } catch (error) {
+        console.log(error);
+        return callRes(res, responseError.TOKEN_IS_INVALID, null);
+    }
 });
+
 // Api sửa phòng chỉ dành cho admin
 router.put('/edit_room', async (req, res) => {
-    const { room_id,token, room_name, max } = req.body;
+    const { token, room_id, room_name, max } = req.body;
 
     // Kiểm tra xem value có hợp lệ hay không
     if(!token || !room_id) return callRes(res, responseError.PARAMETER_VALUE_IS_INVALID,null);
     if(!room_name && !max) return callRes(res, responseError.PARAMETER_VALUE_IS_INVALID,null);
 
-    // Thực hiện cập nhật thông tin phòng trong cơ sở dữ liệu
-    let sql = 'UPDATE rooms SET ';
-    let params = [];
-
-    if (room_name) {
-        sql += 'room_name = ?, ';
-        params.push(room_name);
+    // Kiểm tra xác thực token của admin
+    try {
+        const decoded = jsonwebtoken.verify(token, JWT_SECRET);
+        console.log(decoded);
+        const userId = decoded.userId;
+        const sql = 'SELECT * FROM users WHERE id = ?';
+        connection.query(sql, [userId], (err, results) => {
+            if (err) return callRes(res, responseError.UNKNOWN_ERROR, null);
+            if (results.length === 0) return callRes(res, responseError.USER_IS_NOT_VALIDATED, null);
+            if (results[0].role === 'admin') {
+                // Thực hiện cập nhật thông tin phòng trong cơ sở dữ liệu
+                let sql = 'UPDATE rooms SET ';
+                let params = [];
+            
+                if (room_name) {
+                    sql += 'room_name = ?, ';
+                    params.push(room_name);
+                }
+            
+                if (max) {
+                    sql += 'max = ?, ';
+                    params.push(max);
+                }
+            
+                sql = sql.slice(0, -2);
+                sql += ' WHERE room_id = ?';
+                params.push(room_id);
+            
+                connection.query(sql, params, (err, result) => {
+                    if (err) return callRes(res, responseError.UNKNOWN_ERROR,null);
+            
+                    if (result.affectedRows == 0) return callRes(res, responseError.NO_DATA_OR_END_OF_LIST_DATA,null);
+                    let data = {
+                        room_name : room_name,
+                        max : max,
+                        room_id : room_id
+                    }
+                    return callRes(res, responseError.OK, data);
+                });
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        return callRes(res, responseError.TOKEN_IS_INVALID, null);
     }
-
-    if (max) {
-        sql += 'max = ?, ';
-        params.push(max);
-    }
-
-    sql = sql.slice(0, -2);
-    sql += ' WHERE room_id = ?';
-    params.push(room_id);
-
-    connection.query(sql, params, (err, result) => {
-        if (err) return callRes(res, responseError.UNKNOWN_ERROR,null);
-
-        if (result.affectedRows == 0) return callRes(res, responseError.NO_DATA_OR_END_OF_LIST_DATA,null);
-        let data = {
-            room_name : room_name,
-            max : max,
-            room_id : room_id
-        }
-        return callRes(res, responseError.OK, data);
-    });
 });
 
 export { router };
