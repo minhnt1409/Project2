15ffb0c2b1a640f9887910cf2281b1093f39bda4
NGUYEN NGOC ANH 20194476 <nngocanh697@gmail.com>
2023-05-06 23:44:06 +0700

    api13 + fix connect.js

--- src/db/connect.js
+++ src/db/connect.js
@@ -5,11 +5,11 @@ import {callRes} from '../api/response/response.js';
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: '',
     database: 'api',
 });
 
-connection.connect(error => {
+connection.connect((error, res) => {
     if (error) {
         console.log('Cannot connect to database:', error);
         return callRes(res,responseError.CAN_NOT_CONNECT_TO_DB,null);
