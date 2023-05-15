683f2434a07ccd2a3c31a6950d10a272ff082b4e
NGUYEN NGOC ANH 20194476 <nngocanh697@gmail.com>
2023-05-15 17:02:52 +0700

    api14 + add check condition to api11

--- src/api/route/util.js
+++ src/api/route/util.js
@@ -17,7 +17,10 @@ const JWT_SECRET = 'maBiMat';
 
 // API báo cáo: report
 router.post('/report', async (req, res) => {
-    let { room_id, user_id, content } = req.body;
+    let { token, room_id, user_id, content } = req.body;
+
+    if(!token || !room_id || !user_id || !content) return callRes(res, responseError.PARAMETER_VALUE_IS_INVALID,null);
+
     try {
         // verify token
         const decoded = jsonwebtoken.verify(token, JWT_SECRET);
@@ -36,7 +39,40 @@ router.post('/report', async (req, res) => {
         console.log(error);
         return callRes(res, responseError.TOKEN_IS_INVALID, null);
     }
+});
 
+// API chặn người chơi(chỉ dành cho admin): set_block
+router.post('/set_block', async (req, res) => {
+    let { token, user_id } = req.body;
+
+    if(!token || !user_id) return callRes(res, responseError.PARAMETER_VALUE_IS_INVALID,null);
+
+    try {
+        // verify token
+        const decoded = jsonwebtoken.verify(token, JWT_SECRET);
+        console.log(decoded);
+        
+
+        // check admin role
+        const query = `SELECT role FROM users 
+                        WHERE id = ${user_id}`;
+    
+        connection.query(query, function (error, results, fields) {
+            if (error) return callRes(res, responseError.UNKNOWN_ERROR, null);
+            if(results[0].role != 'admin') return callRes(res, responseError.NOT_ACCESS, null);
+        });
+
+        // update user's state in DB
+        const query2 = `UPDATE users SET is_block = 1 WHERE id = ${user_id}`;
+
+        connection.query(query2, function (error, results, fields) {
+            if (error) return callRes(res, responseError.UNKNOWN_ERROR, null);
+            callRes(res, responseError.OK);
+        });
+    } catch (error) {
+        console.log(error);
+        return callRes(res, responseError.TOKEN_IS_INVALID, null);
+    }
 });
 
 export { router };
