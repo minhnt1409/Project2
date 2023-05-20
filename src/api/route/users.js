f3da54df4793417fac6c502dffb3f15a2cef2a9b
minhnt200409 <khadungok123@gmail.com>
2023-05-21 01:15:08 +0700

    fix

--- src/api/route/users.js
+++ src/api/route/users.js
@@ -8,7 +8,6 @@ import fs from 'fs';
 import validInput from '../utils/validInput.js';
 import responseError from '../response/response.js';
 import { callRes } from '../response/response.js';
-const {getUserIDFromToken} = require('../utils/getUserIDFromToken');
 
 // Import database connection
 import connection from '../../db/connect.js';
